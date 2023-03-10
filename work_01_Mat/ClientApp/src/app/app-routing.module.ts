71ce9db20afe73ecb76f505179c888b09554e4fe
AfsanaSarker <afsanasarker017@gmail.com>
2023-03-11 02:30:35 +0600

    first commit

---
+++ work_01_Mat/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AnimalViewComponent } from './components/animal/animal-view/animal-view.component';
+import { HomeComponent } from './components/home/home.component';
+import { ZooCreateComponent } from './components/zoo/zoo-create/zoo-create.component';
+import { ZooEditComponent } from './components/zoo/zoo-edit/zoo-edit.component';
+import { ZooViewComponent } from './components/zoo/zoo-view/zoo-view.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'zoo', component: ZooViewComponent },
+  { path: 'add-zoo', component: ZooCreateComponent },
+  { path: 'edit-zoo/:id', component: ZooEditComponent },
+  { path: 'animal', component: AnimalViewComponent },
+  { path: '**', component: HomeComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
